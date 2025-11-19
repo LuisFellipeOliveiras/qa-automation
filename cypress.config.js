8093430884e0b6cc5754983748a7140f2ed35d08
LuisFellipeOliveiras <Luislipe1920@gmail.com>
2025-11-19 12:01:37 -0300

    Adjust baseurl

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "https://SEU-USUARIO.github.io/SEU-REPOSITORIO/",
+    baseUrl: "https://github.com/LuisFellipeOliveiras/qa-automation",
     video: false
   }
 });
